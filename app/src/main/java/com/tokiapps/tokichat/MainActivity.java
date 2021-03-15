21b89ba122348701c3b4076ecf265dbbae862162
Alberto <43555310+T0kiii@users.noreply.github.com>
2021-03-15 12:58:42 +0100

    Initial commit

---
+++ app/src/main/java/com/tokiapps/tokichat/MainActivity.java
@@ -0,0 +1,28 @@
+package com.tokiapps.tokichat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+}
