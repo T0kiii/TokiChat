baece0517e716b1cbcc00453b71062483f9d4f3e
Alberto <43555310+T0kiii@users.noreply.github.com>
2021-04-08 21:17:33 +0200

    08_04_21

--- app/src/main/java/com/tokiapps/tokichat/MainActivity.java
+++ app/src/main/java/com/tokiapps/tokichat/MainActivity.java
@@ -3,6 +3,8 @@ package com.tokiapps.tokichat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,6 +12,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private EditText numTelef, codigoSeguridad;
+    private Button enviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
