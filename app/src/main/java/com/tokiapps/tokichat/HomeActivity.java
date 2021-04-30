4c1a03ca72a0532d44ec04cc3b41590b0eecbe3e
Alberto <43555310+T0kiii@users.noreply.github.com>
2021-04-30 09:03:27 +0200

    Autenticación Firebase

---
+++ app/src/main/java/com/tokiapps/tokichat/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.tokiapps.tokichat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button salir = findViewById(R.id.btn_logout);
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // ????
+                finish();
+                return;
+            }
+        });
+    }
+}
--- app/src/main/java/com/tokiapps/tokichat/MainActivity.java
+++ app/src/main/java/com/tokiapps/tokichat/MainActivity.java
@@ -1,13 +1,26 @@
 package com.tokiapps.tokichat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +28,60 @@ public class MainActivity extends AppCompatActivity {
     private EditText numTelef, codigoSeguridad;
     private Button enviar;
 
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
 
-        mAuth = FirebaseAuth.getInstance();
+        userIsLoggedIn();
+
+        numTelef = findViewById(R.id.txt_num_telef);
+        codigoSeguridad = findViewById(R.id.txt_cod_sms);
+        enviar = findViewById(R.id.btn_login);
+
+        enviar.setOnClickListener(v -> startPhoneNumberVerification());
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {}
+        };
+        // mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    userIsLoggedIn();
+                }
+            }
+        });
+    }
+
+    private void userIsLoggedIn() {
+        Toast.makeText(this, "User is connected", Toast.LENGTH_SHORT).show();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            startActivity(new Intent((getApplicationContext()), MainActivity.class));
+            finish();
+            return;
+        }
+    }
+
+    private void startPhoneNumberVerification() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                numTelef.getText().toString(), 60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks);
     }
 
     @Override
